673ab35ec17dd6484f201d47db83472c2659949b
merakana <42410548+merakana@users.noreply.github.com>
2020-10-21 11:09:57 +0530

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,11 @@
+this is a sample text file which contains passwords where password=password, token=token, 
+password="password",
+GMAIL_PASSWORD="gmail",
+***************************
+-----BEGIN RSA PRIVATE KEY-----
+adflkdflksadnflsnlfknslkfnlkasnflknsalkfnsdalkfnlk
+-----END RSA PRIVATE KEY-----
+**********************************
+secret-key="asfdsfdsf";
+secret_key="alsdfdsfsdfsd";
+secret_key=slkfndlfndlfnld;
