c9b3176b3c0ed067122084d11c79d28de603aefa
merakana <42410548+merakana@users.noreply.github.com>
2020-10-21 11:17:04 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,10 +2,11 @@ this is a sample text file which contains passwords where password=password, tok
 password="password",
 GMAIL_PASSWORD="gmail",
 ***************************
------BEGIN RSA PRIVATE KEY----- ************ api_key=asdfldnfldnfl
+-----BEGIN RSA PRIVATE KEY----- GITHUB_API_TOKEN=adflndsflnsdlfnlsnlfsfnldsnf
 adflkdflksadnflsnlfknslkfnlkasnflknsalkfnsdalkfnlk
 -----END RSA PRIVATE KEY-----
 **********************************
 secret-key="asfdsfdsf";
 secret_key="alsdfdsfsdfsd";
 secret_key=slkfndlfndlfnld;
+-----BEGIN RSA PRIVATE KEY----- BEGIN PRIVATE KEY --------------
