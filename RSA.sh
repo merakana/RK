c938c37f53b725f2a99a2ef6f7d3546b15b42238
merakana <42410548+merakana@users.noreply.github.com>
2020-11-25 17:19:24 +0530

    Update RSA.sh

--- RSA.sh
+++ RSA.sh
@@ -2,3 +2,7 @@ private key
 ----- BEGIN CERTIFICATE -----alksfsflhsadfhaslkdfhlkashdflkasdfhlkasfkladsfklasdfhl --- END CERTIFICATE ----
 
 ----- BEGIN CERTIFICATE PRIVATE KEY ----- alksfsflhsadfhaslkdfhlkashdflkasdfhlkasfkladsfklasdfhl --- END CERTIFICATE PRIVATE KEY ----
+
+-----BEGIN CERTIFICATE-----alksfsflhsadfhaslkdfhlkashdflkasdfhlkasfkladsfklasdfhl -----END CERTIFICATE-----
+
+----- BEGIN CERTIFICATE PRIVATE KEY ----- alksfsflhsadfhaslkdfhlkashdflkasdfhlkasfkladsfklasdfhl --- END CERTIFICATE PRIVATE K
