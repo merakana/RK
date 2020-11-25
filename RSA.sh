c099842d3039ec1d4029bda0ef725bd261d194c2
merakana <42410548+merakana@users.noreply.github.com>
2020-11-25 17:25:54 +0530

    Update RSA.sh

--- RSA.sh
+++ RSA.sh
@@ -6,3 +6,12 @@ private key
 -----BEGIN CERTIFICATE-----alksfsflhsadfhaslkdfhlkashdflkasdfhlkasfkladsfklasdfhl -----END CERTIFICATE-----
 
 ----- BEGIN CERTIFICATE PRIVATE KEY ----- alksfsflhsadfhaslkdfhlkashdflkasdfhlkasfkladsfklasdfhl --- END CERTIFICATE PRIVATE K
+
+passphrase=aldskfndslfknf
+
+salt=alksndfsdlkfdsf
+
+encryptionkey=alksdfladflkadsf
+
+
+IV=1232klndfd
